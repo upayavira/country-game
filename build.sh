12dd1289c40a9ba854e19ebca4c5225651237b0b
Upayavira <uv@odoko.co.uk>
2017-01-12 21:44:17 +0000

    Make DB build optional

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
 docker build -t odoko/countries .
-docker rm -f countries countries-db
-docker run --name countries-db -e MYSQL_ROOT_PASSWORD=$DB_PASS -d mysql:5.7
-docker run -e DB_PASS=$DB_PASS odoko/countries init-db
-docker run -d -e GOOGLE_API_KEY=$GOOGLE_API_KEY --name countries -p 8080:8080 odoko/countries app
+docker rm -f countries 
+
+if [ ! -z $DB ]; then
+  docker rm -f countries-db init-db
+  echo "Starting database..."
+  docker run --name countries-db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$DB_PASS -d mysql:5.7
+  echo "Initialising database..."
+  docker run --name init-db -e DB_PASS=$DB_PASS odoko/countries init-db
+fi
+
+docker run  \
+           -e GOOGLE_API_KEY=$GOOGLE_API_KEY \
+	   -e FLASK_DEBUG=$FLASK_DEBUG \
+           --name countries \
+	   -p 8080:8080 \
+	   odoko/countries app
